a10f5181e614e3426555582c737b3bab79d744e1
Shezan Anwar <anwarshezan96@gmail.com>
2026-07-02 17:17:36 +0530

    Refactor DFS function and comments for clarity

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void dfs(int i , vector<vector<int>>& isConnected , vector<bool> &vis){
+    void dfs(int i , vector<vector<int>>& isConnected , vector<bool> &vis){ // normal dfs logic to visit connected vertices
         vis[i]= true ;
         for (int j = 0 ; j < isConnected[i].size() ; j++){
             if(isConnected[i][j]==1 && !vis[j]){
@@ -14,9 +14,9 @@ public:
         int n = isConnected.size();
         vector<bool> vis(n,false);
         for (int i = 0 ; i < n ; i++){
-            if(!vis[i]){
-                dfs(i , isConnected , vis );
-                ans++;
+            if(!vis[i]){    // every unvisited vertex adds up to a new province
+                dfs(i , isConnected , vis ); // check krke update krdenge vertex ko 
+                ans++; // ans me add
             }
         }
         return ans;
