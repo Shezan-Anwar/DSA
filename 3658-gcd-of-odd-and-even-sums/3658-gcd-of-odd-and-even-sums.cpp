743ec401fd663a2bd86e2d150966562d18fce3b2
Shezan Anwar <anwarshezan96@gmail.com>
2026-07-15 19:28:47 +0530

    Time: 0 ms (100%), Space: 8.6 MB (9.11%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    
+    int gcdOfOddEvenSums(int n) {
+        
+        //as oddSum will always be (n*n) & evenSum will be (n*(n+1)) so the  greatest common diviser will be n itself
+        return n;
+        
+    }
+};
