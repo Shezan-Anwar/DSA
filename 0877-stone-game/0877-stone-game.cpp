60e25bfbee40555897c921adad02326dec751044
Shezan Anwar <anwarshezan96@gmail.com>
2026-08-02 11:52:01 +0530

    Time: 3 ms (38.73%), Space: 10.7 MB (38.82%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        // return true;
+        // i could have just returned true , 
+        // but,
+        // with great power , comes great responsibilities - SpiderMan
+
+        int n = piles.size();
+        vector<int> dp = piles;
+
+        for (int len = 2; len <= n; ++len) {
+            for (int i = 0; i <= n - len; ++i) {
+                int j = i + len - 1;
+                dp[i] = max(piles[i] - dp[i + 1], piles[j] - dp[i]);
+            }
+        }
+
+        return dp[0] > 0;
     }
 };
