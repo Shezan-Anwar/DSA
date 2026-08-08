b401a144739f66c3c071632063e051d2e8ec94d6
Shezan Anwar <anwarshezan96@gmail.com>
2026-08-08 20:08:25 +0530

    Time: 3 ms (58.65%), Space: 117.9 MB (94.03%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+    //     stack<int> s;
+    //     ListNode* curr = head;
+
+    //     while (curr != NULL) {
+    //         s.push(curr->val);
+    //         curr = curr->next;
+    //     }
+
+    //     curr = head;
+    //     while (curr != NULL) {
+    //         if (curr->val != s.top()) {
+    //             return false;
+    //         }
+    //         s.pop();
+    //         curr = curr->next;
+    //     }
+
+    //     return true;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+       ListNode* p1 = head;
+        ListNode* p2 = prev;
+        while (p2 != nullptr) {
+            if (p1->val != p2->val) {
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+
+        return true;
+    }
+};
