1c1656d6dfb7d5f454724bb098c01499fbf1abac
Shezan Anwar <anwarshezan96@gmail.com>
2026-08-14 11:08:01 +0530

    Time: 0 ms (100%), Space: 9.8 MB (28.94%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -6,14 +6,14 @@ public:
         int left = 0 ;
         int ans = 0;
         unordered_map<char,int> m;
-        while ( right < n ){
+        for(int right = 0 ; right<n ; right++){
             m[s[right]]++;
             while(m[s[right]]>2){
                 m[s[left]]--;
                 left++;
             }
             ans = max(ans,right-left+1);
-            right++;
+    
         }
         return ans;
     }
