332c412615944265a94b0728d1218fb7b2aeccea
Shezan Anwar <anwarshezan96@gmail.com>
2026-08-23 16:26:04 +0530

    Time: 7 ms (21.45%), Space: 14.1 MB (10.26%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+        int lSum = 0, rSum = 0;
+        int ql = 0 , qr = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(i<n/2){
+                if (num[i]=='?'){
+                    ql++;
+                    continue;
+                }
+                lSum+= num[i]-'0';
+            }else{
+                if (num[i]=='?'){
+                    qr++;
+                    continue;
+                }
+                rSum+= num[i]-'0';
+            }
+        }
+        if (ql+qr == 0) return lSum != rSum;
+        return (2 * (lSum - rSum) + (ql - qr) * 9) != 0;
+        }
+};
