308b6986efd7974452d211c2a0bf9605a0e10fbb
Shezan Anwar <anwarshezan96@gmail.com>
2026-09-03 23:12:59 +0530

    Time: 1 ms (79.58%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n = nums1.size();
+        if(n==1){
+            return true;
+        }
+        int smallestNum = INT_MAX;
+        bool allEven = true;
+        for(int i = 0 ; i < n;i++){
+            if(nums1[i]%2!=0){
+                allEven = false;
+            }
+            smallestNum = min(nums1[i],smallestNum);
+        }
+        if(allEven){
+            return true;
+        }
+        if(smallestNum%2!=0){
+            return true;
+        }
+        return false;
+    }
+};
